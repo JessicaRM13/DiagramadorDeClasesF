7fe43becdbaabe0315368f1d19f7758f0dae2f49
JessicaRM13 <87706307+JessicaRM13@users.noreply.github.com>
2024-10-04 11:15:44 -0400

    Update SocketContext.jsx

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -4,8 +4,8 @@ import { useSocket } from "../hooks/useSocket";
 export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
-    //const url = `https://c4maker-server.herokuapp.com/`;
-    const url = 'http://localhost:8080';
+    const url = `https://diagrammer-backend-024a32c0f637.herokuapp.com/`;
+    //const url = 'http://localhost:8080';
     const { socket, online } = useSocket(url);
 
     return (

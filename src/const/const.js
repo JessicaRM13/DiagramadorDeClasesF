ba771bacae0ab2dda10d6068d60a37db275bd6f4
JessicaRM13 <87706307+JessicaRM13@users.noreply.github.com>
2024-10-04 11:14:49 -0400

    Update const.js

--- src/const/const.js
+++ src/const/const.js
@@ -1,2 +1,2 @@
-export const baseUrl = "http://localhost:8080/api";
-//export const baseUrl = "https://diagrammerdb-nodejs-production.up.railway.app/api";
+//export const baseUrl = "http://localhost:8080/api";
+export const baseUrl = "https://diagrammer-backend-024a32c0f637.herokuapp.com/api";
